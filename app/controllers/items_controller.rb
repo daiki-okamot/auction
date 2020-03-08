f10951bbc991edd0f9dee61e94391e1618eba10e
daiki-okamot <okmtdik0417@icloud.com>
2020-03-08 12:34:37 +0900

    データの登録 new create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,24 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    @item.save
+
+    redirect_to @item
+  end
   
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :sellur, :description, :email, :image_url)
+  end
+
+
 end
